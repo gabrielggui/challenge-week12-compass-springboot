cf77443c1673a313c1c1c2a68ee8b9c6bae20ccc
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-08-29 01:46:00 -0300

    Initial PostController Implementation

--- src/main/java/uol/compass/challenge3/controller/PostController.java
+++ src/main/java/uol/compass/challenge3/controller/PostController.java
@@ -1,21 +1,38 @@
 package uol.compass.challenge3.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import uol.compass.challenge3.entity.Post;
+import uol.compass.challenge3.repository.PostRepository;
 import uol.compass.challenge3.service.PostService;
 
 @RestController
+@RequestMapping("/api/posts")
 public class PostController {
 
     @Autowired
     private PostService postService;
 
-    @PostMapping("/posts/{id}")
-    public String processPost(@PathVariable Long id) {
-        postService.createdPost(id);
-        return "";
+@Autowired
+    private PostRepository postRepository;
+
+    @GetMapping
+    private Iterable<Post> queryPosts() {
+        return postService.findAll();
+    }
+
+    @PostMapping("/{id}")
+    public Post processPost(@PathVariable Long id) {
+        return postService.createPost(id);
+    }
+
+    @GetMapping("/{id}")
+    public Post g(@PathVariable Long id) {
+        return postRepository.findById(id).get();
     }
 }
