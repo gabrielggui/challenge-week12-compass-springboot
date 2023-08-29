2ae76243bc13bd84eb131ee57f6c7d9d776c68f5
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-08-29 13:22:11 -0300

    Inserted "/comments" endpoint in FeignClient

--- src/main/java/uol/compass/challenge3/api/PostApiClient.java
+++ src/main/java/uol/compass/challenge3/api/PostApiClient.java
@@ -1,10 +1,12 @@
 package uol.compass.challenge3.api;
 
+import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import uol.compass.challenge3.entity.Comment;
 import uol.compass.challenge3.entity.Post;
 
 @FeignClient(name = "postsApiClient", url = "https://jsonplaceholder.typicode.com")
@@ -12,4 +14,7 @@ public interface PostApiClient {
 
     @GetMapping("/posts/{id}")
     Post findPostById(@PathVariable Long id);
+
+    @GetMapping("/posts/{postId}/comments")
+    List<Comment> findCommentsByPostId(@PathVariable Long postId);
 }
