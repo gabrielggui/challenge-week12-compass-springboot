8326f8744ea4ea5d2f9406ad5b1d2d813f00d10e
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-08-31 12:13:02 -0300

    implementing caching

--- src/main/java/uol/compass/challenge3/Challenge3Application.java
+++ src/main/java/uol/compass/challenge3/Challenge3Application.java
@@ -2,17 +2,15 @@ package uol.compass.challenge3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
-@EnableScheduling
+@EnableCaching
 @SpringBootApplication
 public class Challenge3Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Challenge3Application.class, args);
-		
 	}
-
 }
